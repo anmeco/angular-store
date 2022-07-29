ddecee1ac6904b0a6e55b39167de5a0e46123c92
amedina <amedina@proxya.com>
2022-07-29 12:05:01 +0200

    Styled Shop, Added Cart (Services and Operable Buttons), Reworork of the App Structure

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,20 +2,35 @@ import { Component, OnInit } from "@angular/core";
 import { Product } from "./interfaces/product.interface";
 import { ProductsService } from "./services/products.service";
 import { tap } from "rxjs/operators";
+import { ShoppingCartService } from "src/app/shared/services/shopping-cart.services";
 
 @Component({
   selector: "app-products",
-  templateUrl: "./products.component.html",
+  template: `<section class="products">
+    <app-product
+      (addToCartClick)="addToCart($event)"
+      [product]="product"
+      *ngFor="let product of products"
+    ></app-product>
+  </section> `,
   styleUrls: ["./products.component.scss"],
 })
 export class ProductsComponent implements OnInit {
   products!: Product[];
-  constructor(private productSvc: ProductsService) {}
+  constructor(
+    private productSvc: ProductsService,
+    private shoppingCartSvc: ShoppingCartService
+  ) {}
 
   ngOnInit(): void {
     this.productSvc
       .getProducts()
       .pipe(tap((products: Product[]) => (this.products = products)))
       .subscribe();
+  }
+
+  addToCart(product: Product): void {
+    console.log("Added to cart :)", product);
+    this.shoppingCartSvc.updateCard(product);
   }
 }
